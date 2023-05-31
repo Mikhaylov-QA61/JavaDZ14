8f6a51730f4695f125f1ad7229a3213be2eebedb
Pers test <qa61test@gmail.com>
2023-05-31 23:26:10 +0400

    Test Exception

--- src/main/java/netology/NotFoundException.java
+++ src/main/java/netology/NotFoundException.java
@@ -1,6 +1,6 @@
 package netology;
 
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String s) {
         super(s);
--- src/main/java/netology/ShopRepository.java
+++ src/main/java/netology/ShopRepository.java
@@ -26,7 +26,14 @@ public class ShopRepository {
      * @param product — добавляемый товар
      */
     public void add(Product product) {
-        products = addToArray(products, product);
+        int compare = product.getId();
+
+        if (findById(compare) == null) {
+            products = addToArray(products, product);
+        } else {
+            throw new RuntimeException("Уже существует продукция с ID: " + compare);
+        }
+
     }
 
     public Product[] findAll() {

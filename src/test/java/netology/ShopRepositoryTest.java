0fcc7f4803d42756be2b405cf1443846fc154881
Pers test <qa61test@gmail.com>
2023-05-31 22:14:00 +0400

    Testing ShopRepository

---
+++ src/test/java/netology/ShopRepositoryTest.java
@@ -0,0 +1,37 @@
+package netology;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class ShopRepositoryTest {
+
+    ShopRepository shop = new ShopRepository();
+    Product product1 = new Product(1, "Хлеб", 10);
+    Product product2 = new Product(22, "Молоко", 60);
+    Product product3 = new Product(333, "Водка", 666);
+    public void setup() {
+        shop.add(product1);
+        shop.add(product2);
+        shop.add(product3);
+    }
+
+    @Test
+    public void removeNullIdTest() {
+        setup();
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            shop.remove(555);
+        });
+    }
+
+    @Test
+    public void removeProductByIDTest() {
+        setup();
+
+        shop.remove(22);
+        Product[] expected = {product1, product3};
+        Product[] actual = shop.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
